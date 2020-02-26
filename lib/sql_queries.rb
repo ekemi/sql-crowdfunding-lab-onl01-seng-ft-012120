bc71d18743e1bfd4503e6db097de4fb60ff570af
Edwin Cyr <edmundopierre@yahoo.com>
2020-02-26 21:15:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   HAVING amount_left >=0;"
 end
 
-def 
+def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "Write your SQL query Here"
 end
 
